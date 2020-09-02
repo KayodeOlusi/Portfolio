d77660a1f3043ee89f48fbbf1449ea005d51ee3f
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2020-09-02 20:48:40 +0100

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,11 @@
+//The Javascript For The Portfolio
+(function($){
+	var typed = new Typed('span.txt-rotate',{
+		strings:["I'm a Web Designer","I'm a Full-Stack Developer"]
+		typeSpeed: 100,
+		backSpeed:100,
+		fadeOut:false,
+		smartBackspace:true,
+		loop:true
+	});
+})(jQuery);
